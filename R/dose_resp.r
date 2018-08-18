0d6c02af06cf83f433e3c7728c15c153aa7e98c4
itamuria <itamuria@gmail.com>
2018-08-18 02:29:47 -0400

    First uploading

---
+++ R/dose_resp.r
@@ -0,0 +1,35 @@
+#' Dose reponse effect all together
+#'
+#' @param allsim dataset with all simulations values
+#' @param dataset dataset with all variables
+#' @param exposures a vector with exposures
+#' @param dr a vector with dose response values
+#' @param ic_dis choose between ic (interval confidences) and dis (distribution)
+#' @param st summary table from general function
+#' @return a data frame with dose response values
+#' @export
+
+
+dose_resp <- function(allsim, dataset, exposures, dr, ic_dis = "IC", st) {
+    st2 <- as.character(st[, 1])
+    un_dr <- unique(grep("DR_", st2, value = TRUE))
+    len_exp <- length(exposures)
+    len_dr <- length(un_dr)
+    df_ace <- data.frame(matrix(NA, len_exp * len_dr, 4))
+    names(df_ace) <- c("Group", "Mean", "ICa", "ICb")
+    b2 <- data.frame(matrix(NA, 1, 4))
+    names(b2) <- c("Quantile", "Mean", "SE", "Exp")
+    h <- 0
+    for (ex in 1:len_exp) {
+        h <- h + 1
+        stp <- st[st$Case == exposures[ex], c(4, 5)]
+        from <- as.numeric(stp[3, 1])
+        to <- as.numeric(stp[dim(stp)[1], 2])
+        mdata <- allsim[from:to, ]
+        b <- dose_resp_ind(allsim = mdata, dataset = dataset, dr = dr)
+        b$Exp <- exposures[ex]
+        b2 <- rbind(b2, b)
+    }
+    b2 <- b2[-1, ]
+    return(b2)
+}
---
+++ R/naive_ace.r
@@ -0,0 +1,31 @@
+#' Naive Average causal effect all together
+#'
+#' @param allsim dataset with all simulations values
+#' @param dataset dataset with all variables
+#' @param exposures a vector with exposures
+#' @param delta a vector with two values
+#' @param ic_dis choose between ic (interval confidences) and dis (distribution)
+#' @param st summary table from general function
+#' @return a data frame with naive ace and confident intervals
+#' @export
+
+
+naive_ace <- function(allsim, dataset, exposures, delta = c(0, 1), ic_dis = "IC", st) {
+    len_exp <- length(exposures)
+    df_ace <- data.frame(matrix(NA, len_exp, 4))
+    names(df_ace) <- c("Group", "Mean", "ICa", "ICb")
+    h <- 0
+    for (ex in 1:len_exp) {
+        h <- h + 1
+        df_ace[h, "Group"] <- paste0(exposures[ex])
+        stp <- st[st$Group == "CC" & st$Case == exposures[ex], c(4, 5)]
+        from <- as.numeric(stp[1, 1])
+        to <- as.numeric(stp[2, 2])
+        mdata <- allsim[from:to, ]
+        b <- naive_ace_ind(allsim = mdata, dataset = dataset, ic_dis = "IC")
+        df_ace[h, "Mean"] <- b[[1]]
+        df_ace[h, "ICa"] <- b[[2]]
+        df_ace[h, "ICb"] <- b[[3]]
+    }
+    return(df_ace)
+}
